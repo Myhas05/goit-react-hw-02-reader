367dbfcd7211a87af9f48cb82dd835117f9ebe6b
Mikhail Fedorovich Umanchuk <mumanchuk@everest.enet.vn.ua>
2019-10-31 16:42:08 +0200

    Updates

---
+++ precache-manifest.2b3e0259049702e96595cb236fba96ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9942cfec35e76d02c58e30a153958082",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "d2dd8bab3948e1cea794",
+    "url": "/goit-react-hw-02-reader/static/css/main.bb6bae19.chunk.css"
+  },
+  {
+    "revision": "6ef2c4717bfdda3784f0",
+    "url": "/goit-react-hw-02-reader/static/js/2.254b4c81.chunk.js"
+  },
+  {
+    "revision": "d2dd8bab3948e1cea794",
+    "url": "/goit-react-hw-02-reader/static/js/main.ea04c896.chunk.js"
+  },
+  {
+    "revision": "0da2320c246f0dcbed6e",
+    "url": "/goit-react-hw-02-reader/static/js/runtime-main.21ebc90b.js"
+  }
+]);
